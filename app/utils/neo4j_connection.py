b6fd4997b3b56e647f1281138739bcd99586ba9b
Alfien Sukma Prawira <alfien.sukmaprawira1724@gmail.com>
2025-06-15 10:41:37 +0700

    fix: deploy

--- app/utils/neo4j_connection.py
+++ app/utils/neo4j_connection.py
@@ -1,19 +1,19 @@
 from neo4j import GraphDatabase
 from dotenv import load_dotenv
 import os
-
+    
 class Neo4jConnection:
     def __init__(self):
-        load_dotenv()
         self.uri = os.getenv("NEO4J_URI", "bolt://localhost:7687")
         self.user = os.getenv("NEO4J_USERNAME", "neo4j")
-        self.password = os.getenv("NEO4J_PASSWORD", "my-password")
+        self.password = os.getenv("NEO4J_PASSWORD", "alfien0310")
         self.driver = None
         self._connect()
 
     def _connect(self):
         try:
             self.driver = GraphDatabase.driver(self.uri, auth=(self.user, self.password))
+            self.driver.verify_connectivity()
         except Exception as e:
             raise Exception(f"Failed to connect to Neo4j: {str(e)}")
 
--- temupaper_app/settings.py
+++ temupaper_app/settings.py
@@ -13,10 +13,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
-load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
